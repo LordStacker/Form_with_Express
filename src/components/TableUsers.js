33f900bf676a06f37cbf996a494859695e3987c3
Nicola Martinez <nicoenrique_1994@hotmail.com>
2021-11-24 20:25:33 -0300

    table responsive and simple design for reading it

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -3,12 +3,13 @@ import { useEffect, useState } from "react";
 import "../styles/table.css";
 
 const TableUsers = () => {
+  //Variable que sostiene el array de usuarios// variable that holds the array of users
   const [existing, setExisting] = useState([]);
 
   useEffect(() => {
     ReadData();
   }, []);
-
+//Lectura de backend usuarios/reading method for users in backend
   const ReadData = () => {
     fetch("http://localhost:3002/users", {
       method: "GET",
@@ -23,13 +24,8 @@ const TableUsers = () => {
       .catch((error) => console.log(error));
     console.log(existing);
   };
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-8 mx-auto">
-          <h1>Table</h1>
-          <div>
-            <table>
+  //Testing code/codigo de testing.
+  /*<table>
               {existing.map((x) => (
                 <tr key={x}>
                   {Object.values(x).map((val) => (
@@ -37,6 +33,32 @@ const TableUsers = () => {
                   ))}
                 </tr>
               ))}
+            </table>*/
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 mx-auto">
+          <h1>Table</h1>
+          <div>
+            <table className="table table-dark table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Names</th>
+                  <th scope="col">Age</th>
+                  <th scope="col">Birth</th>
+                </tr>
+              </thead>
+              <tbody>
+              {existing.map((x) => (
+                <tr key={x}>
+                  {Object.values(x).map((val) => (
+                    <td>{val}</td>
+                  ))}
+                </tr>
+              ))}
+                
+              </tbody>
             </table>
           </div>
         </div>
