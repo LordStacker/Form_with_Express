0cd6d7f22b7e06bb63c3fc395e2c5b2a6b6da763
Nicola Martinez <nicoenrique_1994@hotmail.com>
2021-11-23 23:27:13 -0300

    testing and adding table app for reading whole user list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import './App.css';
-import Login from './components/Login';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./components/Login";
+
 
 function App() {
+  const [existing, setExisting] = useState([]);
+  
+  useEffect(() => {
+    ReadData();
+  }, []);
+
+  const ReadData = () => {
+    fetch("http://localhost:3002/users", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => setExisting(data))
+      .catch((error) => console.log(error));
+      console.log(existing)
+  };
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
-      <Login/>
+      <Login />
+      <div>
+        <h1>Table</h1>
+        <div>
+          <table>
+            {existing.map((x) => (
+              <tr key={x}>
+                {Object.values(x).map((val) => (
+                  <td>{val}</td>
+                ))}
+                </tr>
+            ))}
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
