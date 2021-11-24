84a58febcc0797be861dd5d11a01ef97bf6f54bb
Nicola Martinez <nicoenrique_1994@hotmail.com>
2021-11-24 13:18:28 -0300

    updating app.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Login from "./components/Login";
+import Register from "./components/Register";
+import TableUsers from "./components/TableUsers";
 
 
 function App() {
-  const [existing, setExisting] = useState([]);
   
-  useEffect(() => {
-    ReadData();
-  }, []);
-
-  const ReadData = () => {
-    fetch("http://localhost:3002/users", {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json",
-      },
-    })
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => setExisting(data))
-      .catch((error) => console.log(error));
-      console.log(existing)
-  };
   return (
     <div className="App">
-      <Login />
-      <div>
-        <h1>Table</h1>
-        <div>
-          <table>
-            {existing.map((x) => (
-              <tr key={x}>
-                {Object.values(x).map((val) => (
-                  <td>{val}</td>
-                ))}
-                </tr>
-            ))}
-          </table>
-        </div>
-      </div>
+      <Register />
+      <TableUsers/>
     </div>
   );
 }
